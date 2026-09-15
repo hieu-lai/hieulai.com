9c2dc303586a2e5f2b4d04b8aec4174cad8fc2fd
hieulai <hieuqlai@gmail.com>
2026-09-15 11:58:47 +1000

    ✨ add favicon to root document

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -23,6 +23,10 @@ export const Route = createRootRoute({
         rel: 'stylesheet',
         href: appCss,
       },
+      {
+        rel: 'icon',
+        href: '/favicon.ico',
+      },
     ],
   }),
   shellComponent: RootDocument,
